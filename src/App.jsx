b0785d5d554d031ea2c81479869c98ed98abe2df
akshatkalra <akalra06@students.cs.ubc.ca>
2024-07-25 21:55:57 +0530

    Optimized API fetch which makes the display faster

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,32 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
-  const [fact, setFact] = useState("Meow")
-  // const [imageURL, setImageURL] = useState("🐱")
+  const [fact, setFact] = useState([])
+  const [imageURL, setImageURL] = useState([])
+  const [currentFact, setCurrentFact] = useState("Meow")
+  const [currentImgUrl, setCurrentImgUrl] = useState("")
+
+  useEffect(() => {
+    const fetchFacts = async () => {
+      const response = await fetch('https://meowfacts.herokuapp.com/?count=50', {
+        method: 'GET',
+      });
+      const json = await response.json();
+      setFact(json.data);
+    };
+
+    const fetchImages = async () => {
+      const response = await fetch('https://api.thecatapi.com/v1/images/search?limit=50', {
+        method: 'GET',
+      });
+      const json = await response.json();
+      setImageURL(json);
+    };
+
+    fetchFacts();
+    fetchImages();
+  }, []);
 
 
 
@@ -11,27 +34,15 @@ function App() {
     <>
       <div>
         
-{/*       <img src={imageURL} style={{ width: '300px', height: 'auto' }}/> */}
+      <img src={currentImgUrl} style={{ width: '300px', height: 'auto' }}/>
       
       </div>
-      <h1>{fact}</h1>
+      <h1>{currentFact}</h1>
       <div className="card">
-        <button onClick={
-          async () => {
-            
-            const response = await fetch ('https://meowfacts.herokuapp.com', {
-              method: "GET"
-            })
-
-            const json = await response.json();
-            
-            const fact = json.data[Math.floor(Math.random() * json.data.length)]
-
-            setFact(fact)
-
-            // setImageURL(`https://cataas.com/cat?${Date.now()}`);
-
-          }
+        <button onClick={() => {
+          setCurrentFact(fact[Math.floor(Math.random() * 50)])
+          setCurrentImgUrl(imageURL[Math.floor(Math.random() * imageURL.length)].url)
+        }
         }>
           Click for Cat Fact
         </button>
